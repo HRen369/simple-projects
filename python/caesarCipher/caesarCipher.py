369cab32647c239efaf1fc840f56261358da1834
HRen369 <hrendon369@gmail.com>
2023-10-23 20:59:23 -0700

    added constants to cipher

--- python/caesarCipher/caesarCipher.py
+++ python/caesarCipher/caesarCipher.py
@@ -1,3 +1,20 @@
+ROTATE = 6
+
+LOWER_ALPHABET = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+UPPER_ALPHABET = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+def encrypt(string):
+    pass
+
+def decrypt(string):
+    pass
+
+def testEncrypt():
+    pass
+
+def testDecrypt():
+    pass
+
 def main():
     print("Caesar Cipher")
 
