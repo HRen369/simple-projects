cf75e93e6304c664140e4397d879e3bb5af9f225
HRen369 <hrendon369@gmail.com>
2023-11-05 16:40:07 -0800

    can now move around on the chess board

--- python/chess/chess.py
+++ python/chess/chess.py
@@ -5,15 +5,41 @@ import msvcrt as kb
 
 BACKGROUND_WHITE = "\033[47m"
 ENDC = '\033[0m'
-
-
+SELECTED=  "\033[106m"
 clear = lambda : os.system('cls') if os.name == "nt" else os.system('clear')
 
+NO_MOVE = (-2,-2)
 
 class Piece:
     def __init__(self,color):
         pass
  
+
+def validateBounds(x,y):
+    if x == 8:
+        x -= 1
+    elif x == -1:
+        x += 1
+    elif y == 8:
+        y -= 1
+    elif y == -1:
+        y += 1
+
+    return (x,y)
+
+
+def moveCursor(option, cursorLoc):
+    if option == "w":
+        return validateBounds(cursorLoc[0]-1,cursorLoc[1])
+    elif option == "s":
+        return validateBounds(cursorLoc[0]+1,cursorLoc[1])
+    elif option == "a":
+        return validateBounds(cursorLoc[0],cursorLoc[1]-1)
+    elif option == "d":
+        return validateBounds(cursorLoc[0],cursorLoc[1]+1)
+    else:
+        return NO_MOVE
+
 
 def createBlackPieces():
     return [["♜","♞","♝","♛","♚","♝","♞","♜"],
@@ -30,7 +56,6 @@ def createEmptyPieces():
 def createWhitePieces():
     return [["♙","♙","♙","♙","♙","♙","♙","♙"],
     ["♖","♘","♗","♕","♔","♗","♘","♖"]]
-    
 
 
 def printChessBoard(board,cursor):
@@ -39,23 +64,44 @@ def printChessBoard(board,cursor):
     
     for row in range(len(board)):
         for col in range(len(board[row])):
-            if even % 2 == 0:
-                print(f'{BACKGROUND_WHITE}{board[row][col]} {ENDC}',end="")
-            else:
-                print(f'{board[row][col]} ',end="")
+            if cursor == (row,col):
+                print(f'{SELECTED}', end="")
+            elif even % 2 == 0:
+                print(f'{BACKGROUND_WHITE}',end="")
+            print(f"{board[row][col]} {ENDC}",end="")
             even += 1
-        even += 1
         print()
-
+        even += 1
 
 
 def main():
     clear()
     chessBoard = createBlackPieces() + createEmptyPieces() + createWhitePieces()
-    currentCursor = (0,0)
+    cursorLoc = (0,0)
     turn = 0
+
+    printChessBoard(chessBoard,cursorLoc)
+    with keyboard.Events() as events:
+        for event in events:
+            if type(event) == keyboard.Events.Release and type(event.key) == keyboard._win32.KeyCode:
+                newCurrLoc = moveCursor(event.key.char, cursorLoc)
+                cursorLoc = cursorLoc if newCurrLoc == NO_MOVE else newCurrLoc
+            elif event.key == keyboard.Key.enter and validateMove(board,cursorLoc):
+                board[cursorLoc[0]][cursorLoc[1]] = currentLabel
+                turn += 1
+            elif event.key == keyboard.Key.esc:
+                break
+            
+            printChessBoard(chessBoard,cursorLoc)
+
+    # clears buffer
+    while kb.kbhit(): kb.getch()
+
+    # printChessBoard(board,cursorLoc)
+    # gameOverScreen(haveWon,turn)
     
-    printChessBoard(chessBoard,currentCursor)
+
+    # printChessBoard(chessBoard,currentCursor)
     
     if turn == 0:
         print("***---***---***")   
--- python/tic-tac-toe/ticTacToe.py
+++ python/tic-tac-toe/ticTacToe.py
@@ -6,7 +6,6 @@ BACKGROUND_WHITE = "\033[47m"
 ENDC = '\033[0m'
 
 NO_MOVE = (-2,-2)
-PICKED = (-1,-1)
 
 clear = lambda : os.system('cls') if os.name == "nt" else os.system('clear')
 
