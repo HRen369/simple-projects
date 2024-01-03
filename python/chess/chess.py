374e710274274a7ebd3a407c5a7c7658a2ce65fb
HRen369 <hrendon369@gmail.com>
2024-01-02 20:07:32 -0800

    can now move around board

--- python/chess/chess.py
+++ python/chess/chess.py
@@ -5,7 +5,10 @@ from chessPieces import WhitePawn,WhiteRook,WhiteBishop,WhiteKnight,WhiteQueen,W
 from chessPieces import Empty
 
 LENGTH = 8
+EXIT = (-99,-99)
+
 BG = "\033[47m"
+SEL = "\033[106m"
 ENDC = '\033[0m'
 clear = lambda : os.system('cls') if os.name == "nt" else os.system('clear')
 
@@ -18,24 +21,61 @@ def createChessBoard():
 
     return [blackPieces, blackPiecesPawns,[Empty() for i in range(LENGTH)],[Empty() for i in range(LENGTH)],whitePiecesPawns,whitePieces]
 
-def printChessboard(chessboard):
+def printChessboard(chessboard,currSquare):
     even = 0
     for i in range(len(chessboard)):
         for j in range(len(chessboard[i])):
-            if even % 2 == 0:
+            if currSquare == (i,j):
+                print(f'{SEL}', end="")
+            elif even % 2 == 0:
                 print(f'{BG}',end="")
             print(f"{chessboard[i][j].c} {ENDC}",end="")
             even += 1
         print()
         even += 1
 
-def main():
-    chessboard = createChessBoard()
-    clear()
-    printChessboard(chessboard)
-    # userInput = kb.getch().decode('utf-8')
-    # print(userInput)
 
+def validateBounds(x,y):
+    if x == 8:
+        x -= 1
+    elif x == -1:
+        x += 1
+    elif y == 8:
+        y -= 1
+    elif y == -1:
+        y += 1
+
+    return (x,y)
+
+
+def decodeInput(userInput, currSquare):
+    if userInput == "w":
+        return validateBounds(currSquare[0]-1,currSquare[1])
+    elif userInput == "s":
+        return validateBounds(currSquare[0]+1,currSquare[1])
+    elif userInput == "a":
+        return validateBounds(currSquare[0],currSquare[1]-1)
+    elif userInput == "d":
+        return validateBounds(currSquare[0],currSquare[1]+1)
+    elif userInput == "q":
+        return EXIT
+    else:
+        return currSquare
+
+
+def main():
+    currSquare = (0,0)
+    chessboard = createChessBoard()
+
+    while True:
+        clear()
+        printChessboard(chessboard,currSquare)
+        userInput = kb.getch().decode('utf-8')
+        
+        currSquare = decodeInput(userInput,currSquare)
+        
+        if currSquare == EXIT:
+            break
 
 if __name__ == "__main__":
     main()
