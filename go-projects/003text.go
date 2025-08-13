5ddaac87806cf666a7878f3fd246d599cf190f3e
hrendon369 <hrendon369@gmail.com>
2025-08-12 18:09:35 -0700

    added reverse string

--- go-projects/003text.go
+++ go-projects/003text.go
@@ -28,6 +28,26 @@ func fizzBuzz(num int) {
 	}
 }
 
+/*Reverse a String - Enter a string and the program will reverse it and print it out.*/
+func reverseString(word string)string{
+	reverseStringArray := []rune{}
+
+
+	for i:=len(word)-1;i>=0;i-- {
+		reverseStringArray = append(reverseStringArray, rune(word[i]))
+		
+	}
+	return string(reverseStringArray)
+}
+
+
+/*Pig Latin - Pig Latin is a game of alterations played on the English language game. To create the Pig Latin form of an English word the initial consonant sound is transposed to the end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay). Read Wikipedia for more information on rules.
+
+Count Vowels - Enter a string and the program counts the number of vowels in the text. For added complexity have it report a sum of each vowel found.
+
+Check if Palindrome - Checks if the string entered by the user is a palindrome. That is that it reads the same forwards as backwards like “racecar”
+*/
+
 func main() {
 	// a := 3
 	// fmt.Printf("%v\n", strconv.FormatFloat(piToNthDigit(3), 'f', 3, 64))
@@ -44,6 +64,7 @@ func main() {
 	// fmt.Printf("%v\n", binaryToDecimal(a))
 	// fmt.Printf("%v\n", decimalToBinary(a))
 	// fmt.Printf("%v\n", factorialLoop(a))
-	fizzBuzz(20)
+	// fizzBuzz(20)
+	fmt.Printf("%s\n", reverseString("hello"))
 
 }
